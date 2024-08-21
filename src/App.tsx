4db47f6d818bfcf59ee4677cf3b1eb8bfd006ebc
yuta963 <y.kuromi.arp@gmail.com>
2024-08-21 18:49:41 +0900

    test 8/21

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./Header/Header";
 import { TopPage } from "./Page/TopPage/TopPage";
+import img_FV from "./Image/img_FV.png";
 
 function App() {
   return (
     <>
+      <img src={img_FV} alt="説明文" />
       <Header headerTitle="司法書士法人みどり法務事務所" />
       <TopPage firstQuestion="Q1.現在の借入金額を教えてください" />
     </>
