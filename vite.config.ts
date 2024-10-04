da55856c52283ec00fd7b09789a3dbeedd0497f6
yuta963 <y.kuromi.arp@gmail.com>
2024-10-04 23:46:35 +0900

    公開できるよう修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/diagnosis-midori/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        fallback: "index.html",
-      },
-    },
-  },
 });
